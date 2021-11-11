ca34b0d868b2fccd30e121206366f5091cbe02b4
tushitjain1 <47564813+tushitjain1@users.noreply.github.com>
2021-11-11 12:18:18 +0100

    Delete CompIA directory

--- CompIA/website/user.py
+++
@@ -1,9 +0,0 @@
-class User():
-
-    def __init__(self,name,email,password, active = True):
-        self.name = name
-        self.email = email
-        self.password = password
-        self.active = active
-
-    
