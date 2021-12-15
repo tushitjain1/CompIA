423598a9398d985bd7b24cc49fd469dd6eb2da6b
tushitjain1 <47564813+tushitjain1@users.noreply.github.com>
2021-12-15 12:35:22 +0100

    Delete CompIA directory

--- CompIA/website/algorithm/exams.py
+++
@@ -1,7 +0,0 @@
-class exams:
-    def __init__(self,name,domain=[[],[]],option=0,duration=0,paperNumber=0):
-        self.name = name
-        self.domain = domain
-        self.option = option
-        self.duration = duration
-        self.paperNumber = paperNumber
--- CompIA/website/algorithm/times.py
+++
@@ -1,6 +0,0 @@
-class timess:  
-    def __init__(self,timeStart=0.0,timeEnd=0.0,day=0):
-        self.timeStart = timeStart
-        self.timeEnd = timeEnd
-        self.day = day
-
